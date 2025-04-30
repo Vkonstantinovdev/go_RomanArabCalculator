0dc3299330e145ed9a487b789402e51f55af45c9
Vladislav Konstantinov <vkonstantinovdev@yandex.ru>
2025-05-01 00:03:54 +0300

    update main

--- main.go
+++ main.go
@@ -24,33 +24,33 @@ func add(operator1, operator2 float64) float64 {
 	return operator1 + operator2
 }
 
-func smultiply(operator, operator2 float64) float64 {
-	return operator * operator2
+func multiply(operator1, operator2 float64) float64 {
+	return operator1 * operator2
 }
 
-func subtract(operator, operator2 float64) float64 {
-	return operator - operator2
+func subtract(operator1, operator2 float64) float64 {
+	return operator1 - operator2
 }
 
-func divide(operator, operator2 float64) (float64, error) {
+func divide(operator1, operator2 float64) (float64, error) {
 	if operator2 == 0 {
 		return 0, fmt.Errorf("деление на ноль при операнде %.0f", operator2)
 	}
-	return operator / operator2, nil
+	return operator1 / operator2, nil
 }
 
-func checkOperand(operator, operator2 float64, operand string) (float64, error) {
+func checkOperand(operator1, operator2 float64, operand string) (float64, error) {
 	switch operand {
 	case "+":
-		return add(operator, operator2), nil
+		return add(operator1, operator2), nil
 	case "-":
-		return subtract(operator, operator2), nil
+		return subtract(operator1, operator2), nil
 	case "*":
-		return smultiply(operator, operator2), nil
+		return multiply(operator1, operator2), nil
 	case "/":
-		result, err := divide(operator, operator2)
+		result, err := divide(operator1, operator2)
 		if err != nil {
-			return 0, fmt.Errorf("ошибка при делении %.0f на %.0f: %w", operator, operator2, err)
+			return 0, fmt.Errorf("ошибка при делении %.0f на %.0f: %w", operator1, operator2, err)
 		}
 		return result, nil
 	default:
